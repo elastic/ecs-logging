4dc2ea704bacd8e53314ed74903cf92b3effe8a7
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-01 12:35:55 +0000

    [CI] support sending a PR to each agent when spec changes

---
+++ .ci/scripts/prepare-spec-changes.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -uexo pipefail
+
+readonly REPO_NAME=${1}
+readonly SPECS_TYPE=${2} # json or gherkin
+readonly SPECS_DIR=${3}
+readonly REPO_DIR=".ci/${REPO_NAME}"
+
+EXTENSION="feature"
+if [[ "${SPECS_TYPE}" == "json" ]]; then
+    EXTENSION="json"
+fi
+
+git clone "https://github.com/elastic/${REPO_NAME}" "${REPO_DIR}"
+
+mkdir -p "${REPO_DIR}/${SPECS_DIR}"
+echo "Copying ${EXTENSION} files to the ${REPO_NAME} repo"
+cp spec/*.${EXTENSION} "${REPO_DIR}/${SPECS_DIR}"
+
+cd "${REPO_DIR}"
+git config user.email
+git checkout -b "update-spec-files-$(date "+%Y%m%d%H%M%S")"
+git add "${SPECS_DIR}"
+git commit -m "test: synchronizing ${SPECS_TYPE} specs"
+git --no-pager log -1
