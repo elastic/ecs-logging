c8205f56fdd5248f937f04e43d69216dfd41bab5
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-03 10:22:15 +0000

    Use spec.json file and relative file path in the repos

--- .ci/scripts/prepare-spec-changes.sh
+++ .ci/scripts/prepare-spec-changes.sh
@@ -3,24 +3,20 @@
 set -uexo pipefail
 
 readonly REPO_NAME=${1}
-readonly SPECS_TYPE=${2} # json
-readonly SPECS_DIR=${3}
+readonly SPECS_FILEPATH=${2}
 readonly REPO_DIR=".ci/${REPO_NAME}"
-
-EXTENSION="feature"
-if [[ "${SPECS_TYPE}" == "json" ]]; then
-    EXTENSION="json"
-fi
 
 git clone "https://github.com/elastic/${REPO_NAME}" "${REPO_DIR}"
 
+SPECS_DIR=$(dirname "${SPECS_FILEPATH}")
 mkdir -p "${REPO_DIR}/${SPECS_DIR}"
+
 echo "Copying ${EXTENSION} files to the ${REPO_NAME} repo"
-cp spec/*.${EXTENSION} "${REPO_DIR}/${SPECS_DIR}"
+cp spec/spec.json "${REPO_DIR}/${SPECS_FILEPATH}"
 
 cd "${REPO_DIR}"
 git config user.email
 git checkout -b "update-spec-files-$(date "+%Y%m%d%H%M%S")"
-git add "${SPECS_DIR}"
-git commit -m "test: synchronizing ${SPECS_TYPE} specs"
+git add "${SPECS_FILEPATH}"
+git commit -m "test: synchronizing specs"
 git --no-pager log -1
